91335894ed5a76b982552ff528bb7f9ee3722664
Kaiwalya Koparkar <kaiwalyakoparkar@gmail.com>
2020-01-18 19:23:55 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+import numpy as np
+import imageio
+import scipy.ndimage
+import cv2
+
+img ="photo.jpg"
+
+def grayscale(rgb):
+    return np.dot(...,)
+
+s = imageio.imread(img)
+g = grayscale(s)
